2b0eeca06416313e1498d2f9c19717a7bf5c6e51
visw20 <162998373+visw20@users.noreply.github.com>
2024-07-04 18:08:26 +0530

    Update summarization app.py

--- summarization app.py
+++ summarization app.py
@@ -697,6 +697,15 @@
 
 
 import streamlit as st
+
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
 import re
 import nltk
 import contractions
