e32bdf9882c0db254ddbaea5460f88f3e3003bc5
visw20 <162998373+visw20@users.noreply.github.com>
2024-07-30 16:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,8 @@ nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
 # Load spaCy model
-try:
-    nlp = spacy.load('en_core_web_sm')
-except OSError:
-    from spacy.cli import download
-    download('en_core_web_sm')
-    nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_sm')
+
 
 # Set max_length to a value that accommodates your text length
 nlp.max_length = 5000000  # Set max_length to a value that accommodates your text length
